0d0639382ce7a86929298c238f707bebeb7ffda0
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-03-24 22:22:18 -0600

    install dependecies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import styled from "styled-components";
+
+
+
+const AppContainer = styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App(){
+  return <AppContainer>Awesomenes</AppContainer>
+}
+
+
+export default App;
