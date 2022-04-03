c8464c4a6cd5d39ee8e53a12cdd276641fe5feef
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-04-03 08:06:08 -0600

    Adding Text and Nike Shoe to The Card

--- src/components/nikeCard/index.jsx
+++ src/components/nikeCard/index.jsx
@@ -1,5 +1,7 @@
+import {motion} from "framer-motion";
 import React from "react";
 import styled from "styled-components";
+import JordanImg from "../../assets/air-jordan-transparent.png"
 
 const CardContainer = styled.div`
   width:285px;
@@ -28,7 +30,7 @@ const Circle = styled.div`
   position:absolute;
   width:350px;
   height:350px;
-  top:-4.2rem;
+  top:-4.2em;
   right:-10em;
   z-index:5;
   background-color:#fbbe01;
@@ -53,12 +55,56 @@ const BottomContainer = styled.div`
   padding:0 1em;
 `;
 
+const NikeText = styled.h1`
+  color:#fff;
+  text-transform:uppercase;
+  margin:0;
+  z-index:10;
+  font-size:76px;
+  font-weight:900;
+`;
+
+const ShoesWrapper = styled.div`
+  width:100%;
+  height:100%;
+  position:absolute;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+`;
+
+
+const Shoes = styled(motion.div)`
+  width:auto;
+  height:190px;
+  z-index:99;
+  user-select:none;
+  margin-right:3em;
+  margin-top:4em;
+  img{
+    width:auto;
+    height:100%;
+    user-select:none;
+  };
+
+`;
+
+
 
 
 export function NikeCard(props){
   return <CardContainer>
-          <CircleWrapper>
-            <Circle/>
-          </CircleWrapper>
+            <TopContainer>
+              <CircleWrapper>
+                <Circle/>
+              </CircleWrapper>
+                <ShoesWrapper>
+                  <Shoes style={{rotate:"-25deg"}} >
+                    <img src={JordanImg}/>
+                  </Shoes>
+                </ShoesWrapper>
+                <NikeText>Nike Air</NikeText>
+            </TopContainer>
+            <BottomContainer>bottom</BottomContainer>
         </CardContainer>
 }
